f945972023840c3eaee6b2b95db1cd1be6c81288
bvanhezewijk <bvanhezewijk@gmail.com>
2018-01-24 09:21:11 -0800

    test #5 (username changed)

--- testScript.r
+++ testScript.r
@@ -8,6 +8,8 @@ plot(5,5)  #test #2 from home in Rstudio
 
 plot(6,6)  #test #3 from home in Rstudio
 
-plot(7,7)  #test #4 from work in Rstudio
+plot(7,7)  #test #4 from work in Rstudio using push only
+
+plot(8,8)   #test #5 from work in Rstudio (new username)
 
 temp<-2
